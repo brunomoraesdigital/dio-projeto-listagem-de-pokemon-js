58d04e649730d1c5dc4bebcfaac1cf0ea38f6cdf
Bruno Moraes <brunomoraes.dev@gmail.com>
2024-11-10 01:46:49 -0300

    101124.0145 - inserido as imagens e dados dos pokemons

--- js/script.js
+++ js/script.js
@@ -11,10 +11,23 @@ function getPokemon(valor) {
     const importaImagemPokemon = $("#importaImagemPokemon");
     const impoartaDadosPokemon = $("#impoartaDadosPokemon");
 
-    // Adiciona a imagem do Pokémon com a classe para um ou dois tipos
-    const typeClasses = data.types.map(({ type }) => type.name).join(" ");
+    // Verifica se o Pokémon tem um ou dois tipos
+    const tipo1 = data.types[0]?.type.name || '';  // Tipo 1
+    const tipo2 = data.types[1]?.type.name || '';  // Tipo 2 (caso exista)
+
+    // Condicional para um ou dois tipos
+    if (data.types.length === 1) {
+      content += `<div class="todo ${tipo1}"></div>`; // Quando tem apenas um tipo
+    } else {
+      content += `
+        <div class="meia-esquerda ${tipo1}"></div>
+        <div class="meia-direita ${tipo2}"></div>
+      `; // Quando tem dois tipos
+    }
+
+    // Adiciona a imagem do Pokémon
     content += `
-      <div class="imagemPokemon ${typeClasses}">
+      <div class="imagemPokemon">
         <img alt="" src="${data.sprites.other.dream_world.front_default}">
       </div>
     `;
@@ -34,11 +47,11 @@ function getPokemon(valor) {
 
     // Adiciona o ID, nome, altura, peso e tipos do Pokémon
     content = `
-      <p>Número: ${data.id}</p>
-      <p>Nome: ${data.name}</p>
-      <p>Altura: ${alturaFormatada}</p>
-      <p>Peso: ${pesoFormatado}</p>
-      <p>Tipo: ${data.types.map(({ type }) => type.name).join(" / ")}</p>
+      <p><span>Número</span><span>${data.id}</span></p>
+      <p><span>Nome</span><span>${data.name}</span></p>
+      <p><span>Altura</span><span>${alturaFormatada}</span></p>
+      <p><span>Peso</span><span>${pesoFormatado}</span></p>
+      <p><span>Tipo</span><span>${data.types.map(({ type }) => type.name).join(" / ")}</span></p>
     `;
     impoartaDadosPokemon.html(content);
   })
